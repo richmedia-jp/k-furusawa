157a276600ad30ac6edac1018298089c6fe1adee
moschan <kaityo02@gmail.com>
2014-03-10 15:13:18 +0900

    ビューとコントローラーを実装

---
+++ core/Controller.php
@@ -0,0 +1,87 @@
+<?php
+
+/*==========================
+    コントローラー
+==========================*/
+
+abstract class Controller
+{
+    protected $controller_name;
+    protected $action_name;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $auth_actions = array();
+
+    // コンストラクタ
+    public function __construct($application)
+    {
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request     = $application->getRequest();
+        $this->response    = $application->getResponse();
+    }
+
+    // アクションの実行
+    public function run($action, $params = array())
+    {
+        $this->action_name = $action;
+
+        $action_method = $action . 'Action';
+        if (!method_exists($this, $action_method)) {
+            $this->forward404();
+        }
+
+        if ($this->needsAuthentication($action) && !$this->session->isAuthenticated()) {
+            throw new UnauthorizedActionException();
+        }
+
+        $content = $this->$action_method($params);
+
+        return $content;
+    }
+
+
+    // ビューのレンダリング
+    protected function render($variables = array(), $template = null, $layout = 'layout')
+    {
+        $defaults = array(
+            'request'  => $this->request,
+            'base_url' => $this->request->getBaseUrl(),
+            'session'  => $this->session,
+        );
+
+        $view = new View($this->application->getViewDir(), $defaults);
+
+        if (is_null($template)) {
+            $template = $this->action_name;
+        }
+
+        $path = $this->controller_name . '/' .$template;
+
+        return $view->render($path, $variables, $layout);
+    }
+
+    // 404を出力
+    protected function forward404()
+    {
+        throw new HttpNotFoundException('Forwarded 404 page from '
+            . $this->controller_name . '/' . $this->action_name);
+    }
+
+    // 指定されたURLへリダイレクト
+    protected function redirect($url)
+    {
+        if (!preg_match('#https?://#', $url)) {
+            $protocol = $this->request->isSsl() ? 'https://' : 'http://';
+            $host = $this->request->getHost();
+            $base_url = $this->request->getBaseUrl();
+
+            $url = $protocol . $host . $base_url . $url;
+        }
+
+        $this->response->setStatusCode(302, 'Found');
+        $this->response->setHttpHeader('Location', $url);
+    }
+}
