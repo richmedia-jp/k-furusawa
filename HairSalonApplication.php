4162728ef17a8be2ab78ccd8a67ad561ca7fad41
moschan <kaityo02@gmail.com>
2014-03-10 15:58:11 +0900

    今回のアプリ用のコントローラを実装

---
+++ HairSalonApplication.php
@@ -0,0 +1,47 @@
+<?php
+
+/*==========================
+
+    全体管理
+
+==========================*/
+
+class HairSalonApplication extends Application
+{
+
+    public function getRootDir()
+    {
+        return dirname(__FILE__);
+    }
+
+
+    // ルーティング定義配列
+    protected function registerRoutes()
+    {
+        return array(
+            '/'
+                => array('controller' => 'root', 'action' => 'index'),
+            '/salon'
+                => array('controller' => 'salon', 'action' => 'index'),
+            '/salon/:salon_id'
+                => array('controller' => 'salon', 'action' => 'detail'),
+            '/salon/search/:query'
+                => array('controller' => 'salon', 'action' => 'search'),
+            '/beautician'
+                => array('controller' => 'beautician', 'action' => 'index'),
+            '/beautician/:beautician_id'
+                => array('controller' => 'beautician', 'action' => 'detail'),
+            '/beautician/search/:query'
+                => array('controller' => 'beautician', 'action' => 'detail'),
+        );
+    }
+
+    // protected function configure()
+    // {
+    //     $this->db_manager->connect('master', array(
+    //         'dsn'      => 'mysql:dbname=mini_blog;host=localhost',
+    //         'user'     => 'root',
+    //         'password' => '',
+    //     ));
+    // }
+}
