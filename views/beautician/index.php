ca5aa317f0d189bb43961dc454e13686a3e08b5b
moschan <kaityo02@gmail.com>
2014-03-12 13:12:18 +0900

    美容師検索画のスタイルを軽く当てた

--- views/beautician/index.php
+++ views/beautician/index.php
@@ -1,1 +1,35 @@
-<p>beauticianのindexです</p>
+
+<h1>美容師名から探す</h1>
+<form >
+   <div class="row collapse">
+      <div class="small-10 columns">
+        <input type="text" placeholder="美容師名を入力してください">
+      </div>
+      <div class="small-2 columns">
+        <a href="#" class="button postfix"><i class="fa fa-search"></i></a>
+      </div>
+  </div>
+</form>
+<p><i class="fa fa-user"></i> おすすめの美容師</p>
+
+<ul class="" data-orbit>
+    <li data-orbit-slide="headline-1">
+      <div>
+        <h2>美容師１です</h2>
+        <img src="<?php echo $base_url; ?>/img/shop.png">
+        <a href="#" class="button">トップ</a>
+      </div>
+    </li>
+    <li data-orbit-slide="headline-2">
+      <div>
+        <h2>Headline 2</h2>
+        <h3>Subheadline</h3>
+      </div>
+    </li>
+    <li data-orbit-slide="headline-3">
+      <div>
+        <h2>Headline 3</h2>
+        <h3>Subheadline</h3>
+      </div>
+    </li>
+</ul>
---
+++ views/beautician/search.php
@@ -0,0 +1,62 @@
+<h1>
+  <i class="fa fa-search"></i>
+  キーワード: うんてゃら
+</h1>
+
+
+<div class="search-results">
+
+  <div class="beautician-search-result">
+
+      <div class="beautician-img">
+        <img src="http://placehold.it/200x200/A92B48/fff?text=beautician_img">
+      </div>
+
+      <div class="beautician-caption">
+        <h3><a href="#"><i class="fa fa-male"></i>ニックネーム</a></h3>
+        <label>本名/ほんみょう</label>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+  </div>
+
+    <div class="beautician-search-result">
+
+      <div class="beautician-img">
+        <img src="http://placehold.it/200x200/A92B48/fff?text=beautician_img">
+      </div>
+
+      <div class="beautician-caption">
+        <h3><a href="#"><i class="fa fa-male"></i>ニックネーム</a></h3>
+        <label>本名/ほんみょう</label>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+  </div>
+
+    <div class="beautician-search-result">
+
+      <div class="beautician-img">
+        <img src="http://placehold.it/200x200/A92B48/fff?text=beautician_img">
+      </div>
+
+      <div class="beautician-caption">
+        <h3><a href="#"><i class="fa fa-male"></i>ニックネーム</a></h3>
+        <label>本名/ほんみょう</label>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+  </div>
+</div>
