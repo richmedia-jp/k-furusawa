cd8d3fe180e68286f20430c29179f93c076bfa43
moschan <kaityo02@gmail.com>
2014-03-12 12:51:58 +0900

    美容室詳細ページにスタイルを軽く当てました

--- views/salon/detail.php
+++ views/salon/detail.php
@@ -1,1 +1,66 @@
-<p>美容室の詳細画面です</p>
+
+<div class="salon-detail">
+	<h1><img src="<?php echo $base_url; ?>/img/shop.png">美容室名</h1>
+	  <div class="salon-content">
+
+	      <div class="salon-img">
+	        <img src="http://placehold.it/200x200/A92B48/fff?text=salon_image1">
+	        <div class="salon-imgs">
+		        	<img src="http://placehold.it/85x85/A92B48/fff?text=salon_image1">
+		        	<img src="http://placehold.it/85x85/A92B48/fff?text=salon_image2">
+		        	<img src="http://placehold.it/85x85/A92B48/fff?text=salon_image3">
+	        </div>
+	      </div>
+
+
+	      <div class="salon-caption">
+
+	      		<ul class="salon-tags">
+	      	    <li><a href="#">タグ1</a></li>
+	      	    <li><a href="#">タグ2</a></li>
+	      	    <li><a href="#">タグ3</a></li>
+	      	  </ul>
+
+	      	  <section>
+	      			        	<h3>店舗紹介タイトル</h3>
+	      			        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      			        	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      			        	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      			        	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      			        	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      			        	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      	  </section>
+
+	      	  <section>
+	      	  	<h3>美容師の皆さん</h3>
+	      	  	<ul class="salon__beauticians">
+	      	  					<li class="salon__beautician">
+	      	  						<img src="http://placehold.it/100x100/A92B48/fff?text=beautician_img">
+	      	  						<label>美容師名</label>
+	      	  					</li>
+	      	  					<li class="salon__beautician">
+	      	  						<img src="http://placehold.it/100x100/A92B48/fff?text=beautician_img">
+	      	  						<label>美容師名</label>
+	      	  					</li>
+	      	  					<li class="salon__beautician">
+	      	  						<img src="http://placehold.it/100x100/A92B48/fff?text=beautician_img">
+	      	  						<label>美容師名</label>
+	      	  					</li>
+	      	  	</ul>
+	      	  </section>
+
+	      	  <section>
+	      	  		<label><i class="fa fa-home"></i> 住所</label>
+	      	  		<p>福島県会津若松市</p>
+	      	  		<label><i class="fa fa-clock-o"></i>営業時間</label>
+	      	  		<p>10:00 ~ 19:00</p>
+	      	  		<label><i class="fa fa-frown-o"></i>定休日</label>
+	      	  		<p>第二第四月曜日</p>
+	      	  		<label><i class="fa fa-phone-square"></i> 電話番号</label>
+	      	  		<p>0000-00-0000</p>
+	      	  </section>
+
+	      </div>
+
+	  </div>
+</div>
